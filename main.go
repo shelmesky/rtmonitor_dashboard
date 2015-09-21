3eb18bd1efe76ec4347f7bfe6a146580ace82378
leeway <liwei@bolo.me>
2015-09-21 14:40:54 +0800

    add godep to project

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/shelmesky/rtmonitor_dashboard/Godeps/_workspace/src/github.com/astaxie/beego"
 	_ "github.com/shelmesky/rtmonitor_dashboard/routers"
-	"github.com/astaxie/beego"
 )
 
 func main() {
